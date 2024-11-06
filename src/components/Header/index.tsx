6041f548e9c34b5d7fcf8b58833fda0e985d5e00
NevenaAl <51334038+NevenaAl@users.noreply.github.com>
2024-11-06 15:01:02 +0100

    added scss files

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,21 @@
 import style from './Header.module.scss';
+import Link from '@mui/material/Link';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
+import logo from '../../assets/images/logo.png';
 
 function Header() {
   return (
-    <header className={style.test}>
-      <h1>My App</h1>
+    <header className={style.header}>
       <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/product/3">About</a>
-          </li>
-        </ul>
+        <div className={style.navLinks}>
+          <Link href="/" aria-label="Home">
+            <img width={'150px'} src={logo}></img>
+          </Link>
+          <IconButton color="primary" aria-label="Shopping cart">
+            <ShoppingCartIcon />
+          </IconButton>
+        </div>
       </nav>
     </header>
   );
